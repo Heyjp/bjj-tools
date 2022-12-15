cf7f2de8487e28f4c291d61a52603724470ad9ba
RWX <jplayer@tuta.io>
2022-12-15 15:34:53 +0000

    readded chapters

---
+++ chapters/cmd/chapters/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	c "github.com/heyjp/bjj-tools/chapters"
+	p "github.com/heyjp/bjj-tools/crawler"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: main <crawl|chapters|search>")
+	}
+	switch os.Args[1] {
+	case "crawl":
+		if len(os.Args) < 3 {
+			log.Fatal("Usage: main crawl <all|new>")
+		}
+		siteKey := os.Args[2]
+		p.Crawl(siteKey)
+	case "chapters":
+		c.LoopThroughProducts()
+	case "search":
+		if len(os.Args) < 3 {
+			log.Fatal("Usage: main search <fanatics-product-link>")
+		}
+		product := os.Args[2]
+		folder, err := os.Getwd()
+		if err != nil {
+			log.Fatal(err)
+		}
+		location := folder + "/" + "chapters"
+		c.PrepareChapterFiles(product, location, false)
+	// case "merge":
+	// fmt.Println("Enter in a folder location containing videos")
+	default:
+		fmt.Println("main <search|crawl|chapters|merge>")
+	}
+}
---
+++ chapters/go.mod
@@ -0,0 +1,18 @@
+module github.com/heyjp/bjj-tools/chapters
+
+go 1.18
+
+require (
+	github.com/heyjp/bjj-tools/crawler v0.0.0-20221215152427-ad84c87e0f46
+	github.com/heyjp/bjj-tools/dircheck v0.0.0-20221215152427-ad84c87e0f46
+	github.com/heyjp/bjj-tools/search v0.0.0-20221215152427-ad84c87e0f46
+	github.com/heyjp/bjj-tools/timestamps v0.0.0-20221215152427-ad84c87e0f46
+)
+
+require (
+	github.com/anaskhan96/soup v1.2.5 // indirect
+	github.com/heyjp/bjj-tools/crawler v0.0.0-20221209153059-4993aede89cc // indirect
+	github.com/heyjp/bjj-tools/search v0.0.0-20221209153059-4993aede89cc // indirect
+	golang.org/x/net v0.4.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
+)
